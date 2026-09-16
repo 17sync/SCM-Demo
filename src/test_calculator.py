da83e14d8ee6ade75ac23f801ab27448e37214d9
r1v ubuntu <actuallyaun@gmail.com>
2026-09-16 20:32:10 +0500

    reduced space between functions by 1 line

--- src/test_calculator.py
+++ src/test_calculator.py
@@ -3,6 +3,5 @@ from calculator import calculate_discount
 def test_regular_customer():
     assert calculate_discount(100, False)==10
 
-
 def test_member_customer():
     assert calculate_discount(100, True)==20
